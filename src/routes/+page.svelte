d8a51d1068188d5cce9014afc5973020b27739d1
Alex Bates <alex@nanaian.town>
2024-01-20 19:15:09 +0000

    add button component and move to svelte 4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,14 @@
+<script lang="ts">
+  import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1 class="text-3xl font-bold underline bg-red">
   Hello world!
+
+  <Button variant="outline" on:click={() => alert("Hello world!")}>
+    Click me
+  </Button>
 </h1>
