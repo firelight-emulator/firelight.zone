b0d35c20a66f95d403ae61ef65fcb64b9f4666b6
Alex Bates <alex@nanaian.town>
2024-01-22 17:18:17 +0000

    add hack page

---
+++ src/lib/components/patcher/download.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+  import { Download } from "radix-icons-svelte";
+
+  export let url: string;
+
+  // TODO
+</script>
+
+<Button>
+  <Download class="mr-2" />
+  Download
+</Button>
