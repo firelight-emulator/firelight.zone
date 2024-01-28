f66c9e3bca376be67d7fb3d35ce3ebd8a79f730c
Alex Bates <alex@nanaian.town>
2024-01-28 22:59:53 +0000

    wip electric-sql usage

--- shell.nix
+++ shell.nix
@@ -4,11 +4,35 @@ pkgs.mkShell {
   packages = with pkgs; [
     nodejs
     nodePackages.pnpm
+    prisma-engines
   ];
 
   shellHook = ''
-    pnpm install
+    pnpm install --frozen-lockfile
+
+    # Supabase
     pnpm supabase start
-    trap 'pnpm supabase stop' EXIT
+    pnpm supabase status -o env > .env
+
+    # Make some vars available to SvelteKit client by prefixing with PUBLIC_
+    sed -i 's/ANON_KEY/PUBLIC_ANON_KEY/' .env
+    sed -i 's/API_URL/PUBLIC_API_URL/' .env
+
+    # Load environment variables
+    set -a
+    source .env
+    set +a
+
+    # Prisma (ElectricSQL client dependency) needs binaries provided
+    # !! prisma-engines version must match the version in package.json !!
+    export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+    export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+    export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+    export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+  
+    # ⚡ ElectricSQL
+    export LOGICAL_PUBLISHER_HOST=localhost
+    pnpm electric-sql start --detach
+    trap 'pnpm electric-sql stop && pnpm supabase stop' EXIT
   '';
 }
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,10 @@
-import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
+import { PUBLIC_ANON_KEY, PUBLIC_API_URL } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 
 export const handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl: PUBLIC_ANON_KEY,
+		supabaseKey: PUBLIC_API_URL,
 		event,
 	});
 
