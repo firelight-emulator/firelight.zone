669ac3c6fff60210ce78361e08d90f86ecd5667a
Alex Bates <alex@nanaian.town>
2024-01-29 04:43:08 +0000

    remove platform pages

--- src/routes/platforms/[platform]/+page.server.ts
+++
@@ -1,9 +0,0 @@
-export async function load({ locals: { supabase }, params }) {
-	const { data, error: queryError } = await supabase.
-
-	if (queryError) {
-		console.error(queryError);
-	}
-
-	return { platformGames: data };
-}
