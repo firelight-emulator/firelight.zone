94a3e4ddf6ac0d1199dd119ac8145d76255a3bf1
Alex Bates <alex@nanaian.town>
2024-01-29 16:09:35 +0000

    basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Layout from '$lib/components/layout.svelte';
+</script>
+
+<!-- TODO: style this like a libultra crash screen -->
+
+<Layout centered>
+	<div>
+		<h1 class="text-3xl font-bold">{$page.status} {$page.error?.message}</h1>
+	</div>
+</Layout>
